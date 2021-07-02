9933592f4a7a5b04598996abd667055f5f90dcbf
Ankit Singh <singh.ankit.in@gmail.com>
2021-07-02 23:23:52 +0100

    Adding brute force solution

--- algorithms/src/main/java/FindPairWithSum.java
+++ algorithms/src/main/java/FindPairWithSum.java
@@ -1,7 +1,26 @@
 public class FindPairWithSum {
 
-    boolean findPairWithSum(final int[] array, int sum) {
-        return false;
+    boolean findPairWithSum(int[] array, int sum) {
+        boolean retVal = false;
+        // - brute force - O(N*N)
+        retVal = nestedLoop(array, sum);
+        return retVal;
+    }
+
+    boolean nestedLoop(int[] array, int sum) {
+        boolean retVal = false;
+        if (array != null && array.length > 1) {
+            for (int i = 0; i < array.length; i++) {
+                for (int j = 1; j < array.length; j++) {
+                    //System.out.println(String.format("i:%s, j:%s", i, j));
+                    if (array[i] + array[j] == sum) {
+                        retVal = true;
+                        break;
+                    }
+                }
+            }
+        }
+        return retVal;
     }
 
 }
--- algorithms/src/test/java/FindPairWithSumTest.java
+++ algorithms/src/test/java/FindPairWithSumTest.java
@@ -14,11 +14,13 @@ public class FindPairWithSumTest {
     }
     @Test
     public void testFinfPairWithSum() {
-        Assertions.assertTrue(target.findPairWithSum(new int[]{1,2,3,4,5}, 5));
-        Assertions.assertTrue(target.findPairWithSum(new int[]{0}, 5));
-        Assertions.assertTrue(target.findPairWithSum(new int[]{0,1}, 5));
-        Assertions.assertTrue(target.findPairWithSum(new int[]{6,7,8,9}, 5));
-        Assertions.assertTrue(target.findPairWithSum(new int[]{-1,0,1,2}, 5));
+        Assertions.assertFalse(target.findPairWithSum(new int[]{0}, 5));
+        Assertions.assertFalse(target.findPairWithSum(new int[]{0,1}, 5));
+        Assertions.assertFalse(target.findPairWithSum(new int[]{6,7,8,9}, 5));
+        Assertions.assertFalse(target.findPairWithSum(new int[]{-1,0,1,2}, 5));
         Assertions.assertTrue(target.findPairWithSum(new int[]{-1,0,6,2}, 5));
+        Assertions.assertTrue(target.findPairWithSum(new int[]{1,2,3,4,5}, 5));
+        Assertions.assertTrue(target.findPairWithSum(new int[]{4,4}, 8));
+        Assertions.assertFalse(target.findPairWithSum(new int[]{4}, 4));
     }
 }
