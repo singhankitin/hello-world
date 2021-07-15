53d5efa516ecccc4181be67d4604a46c45cf4be4
Ankit Singh <singh.ankit.in@gmail.com>
2021-07-15 10:16:06 +0100

    Reversing a string using recursion

--- algorithms/src/main/java/ReverseAString.java
+++ algorithms/src/main/java/ReverseAString.java
@@ -1,13 +1,6 @@
 public class ReverseAString {
 
-    public String reverseAString(String input) {
-        String retVal;
-        // Simplest
-        retVal = reverseByIteratingArray(input); // O(N)
-        return retVal;
-    }
-
-    private String reverseByIteratingArray(String input) {
+    public String reverseByIteratingArray(String input) {
         StringBuilder retVal = new StringBuilder();
         if (input != null && input.length() > 0) {
             char[] chars = input.toCharArray();
@@ -17,4 +10,22 @@ public class ReverseAString {
         }
         return retVal.toString();
     }
+
+    // abcdef
+    // f(bcdef)                             + a
+    // f(cdef)                      + b     + a
+    // f(def)                   +c  + b     + a
+    // f(ef)            + d     +c  + b     + a
+    // f(f)         + e + d     +c  + b     + a
+    // f(f)         + e + d     +c  + b     + a
+    // f("")    +f  + e + d     +c  + b     + a
+    // ""       +f  + e + d     +c  + b     + a
+    public String reverseUsingRecursion(String input) {
+        if (input == null || input.isEmpty()) {
+            return "";
+        }
+        System.out.printf("\nreverseUsingRecursion([%s]) + [%s]", input.substring(1), input.charAt(0));
+        return reverseUsingRecursion(input.substring(1)) + input.charAt(0);
+    }
+
 }
--- algorithms/src/test/java/ReverseAStringTest.java
+++ algorithms/src/test/java/ReverseAStringTest.java
@@ -5,18 +5,24 @@ import org.junit.jupiter.params.provider.CsvSource;
 
 public class ReverseAStringTest {
     ReverseAString target = new ReverseAString();
+
     @Test
     public void testWithNull() {
-        Assertions.assertEquals("", target.reverseAString(null));
+        Assertions.assertEquals("", target.reverseByIteratingArray(null));
+        Assertions.assertEquals("", target.reverseUsingRecursion(null));
     }
+
     @Test
     public void testWithEmptyString() {
-        Assertions.assertEquals("", target.reverseAString(""));
+        Assertions.assertEquals("", target.reverseByIteratingArray(""));
+        Assertions.assertEquals("", target.reverseUsingRecursion(""));
     }
+
     @ParameterizedTest
-    @CsvSource({"abc,cba", "a,a"})
-    public void testWithEmpty(String input, String expected) {
-        Assertions.assertEquals(expected, target.reverseAString(input));
+    @CsvSource({"abc,cba", "a,a", "abcdef, fedcba"})
+    public void test(String input, String expected) {
+        Assertions.assertEquals(expected, target.reverseByIteratingArray(input));
+        Assertions.assertEquals(expected, target.reverseUsingRecursion(input));
     }
 
 
