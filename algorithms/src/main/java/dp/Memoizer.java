84c3ca464599358ac656db9d7692fdcfe7d93a7a
Ankit Singh <singh.ankit.in@gmail.com>
2021-07-16 21:18:35 +0100

    Fibonacci using memoization

--- algorithms/src/main/java/dp/Memoizer.java
+++ algorithms/src/main/java/dp/Memoizer.java
@@ -1,13 +1,55 @@
 package dp;
 
+import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.Function;
 
 public class Memoizer {
 
-    Function<Long, Long> factorial() {
+    private int computationCount;
+
+    public Function<Long, Long> fibonacci() {
+        return new Function<Long, Long>() {
+            final Map<Long, Long> cache = new ConcurrentHashMap<>();
+
+            @Override
+            public Long apply(Long index) {
+                Long factorial = fibonacciRecursive(index, cache);
+                System.out.printf("\nComputation count:%s", computationCount);
+                return factorial;
+            }
+        };
+    }
+
+    private Long fibonacciRecursive(Long index, Map<Long, Long> cache) {
+        if (index > 0) {
+            if (index < 3) {
+                if (cache.containsKey(index)) {
+                    return cache.get(index);
+                } else {
+                    computationCount++;
+                    cache.put(index, index - 1);
+                    return index - 1;
+                }
+            } else {
+                if (cache.containsKey(index)) {
+                    return cache.get(index);
+                } else {
+                    computationCount++;
+                    long fibonacci = fibonacciRecursive(index - 1L, cache) + fibonacciRecursive(index - 2L, cache);
+                    cache.put(index, fibonacci);
+                    return fibonacci;
+                }
+            }
+        } else {
+            throw new IllegalArgumentException("Index should be greater than zero");
+        }
+    }
+
+    public Function<Long, Long> factorial() {
         return new Function<Long, Long>() {
             private final ConcurrentHashMap<Long, Long> cache = new ConcurrentHashMap<>();
+
             @Override
             public Long apply(Long value) {
                 System.out.printf("\n[%s]", cache);
--- algorithms/src/test/java/dp/MemoizerTest.java
+++ algorithms/src/test/java/dp/MemoizerTest.java
@@ -10,7 +10,45 @@ public class MemoizerTest {
     private Memoizer target = new Memoizer();
 
     @Test
-    public void test() {
+    public void testFibonacci() {
+        Function<Long, Long> fibonacci = target.fibonacci();
+        Assertions.assertThrows(IllegalArgumentException.class, () -> fibonacci.apply(-1L));
+        Assertions.assertEquals(0, fibonacci.apply(1L));
+        Assertions.assertEquals(1, fibonacci.apply(2L));
+        Assertions.assertEquals(1, fibonacci.apply(3L));
+        Assertions.assertEquals(2, fibonacci.apply(4L));
+        Assertions.assertEquals(3, fibonacci.apply(5L));
+        Assertions.assertEquals(5, fibonacci.apply(6L));
+        Assertions.assertEquals(8, fibonacci.apply(7L));
+        Assertions.assertEquals(13, fibonacci.apply(8L));
+        Assertions.assertEquals(21, fibonacci.apply(9L));
+        Assertions.assertEquals(34, fibonacci.apply(10L));
+//        *Without* caching
+//        Computation count:2
+//        Computation count:3
+//        Computation count:6
+//        Computation count:11
+//        Computation count:20
+//        Computation count:35
+//        Computation count:60
+//        Computation count:101
+//        Computation count:168
+//        Computation count:277
+//        *With* caching
+//        Computation count:1
+//        Computation count:2
+//        Computation count:3
+//        Computation count:4
+//        Computation count:5
+//        Computation count:6
+//        Computation count:7
+//        Computation count:8
+//        Computation count:9
+//        Computation count:10
+    }
+
+    @Test
+    public void testFactorial() {
         Function<Long, Long> factorial = target.factorial();
         Assertions.assertThrows(IllegalArgumentException.class, () -> factorial.apply(-1L));
         Assertions.assertEquals(2L, factorial.apply(2L));
